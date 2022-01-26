53b5e2ab46d1f961c2f1b9bd53cfed43600c9476
阿豪boy <504595380@qq.com>
2022-01-26 19:12:22 +0800

    fix: init

--- src/index.tsx
+++ src/index.tsx
@@ -149,28 +149,29 @@ const saveImg = async (data, width, height) => {
 };
 const Counter = () => {
   const [data, setData] = useState();
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
+  const [width, setWidth] = useState(256);
+  const [height, setHeight] = useState(240);
+  const cx = 2;
+  const cy = 4;
+  const mw = width / cx;
+  const mh = height / cy;
+
   useEffect(() => {
     async function main() {
       let st = +new Date();
       const rom = new Uint8Array(Buffer.from(island_3_base64, "base64"));
+      console.log("rom");
       const render = async (data: Uint8Array) => {
         const end = +new Date();
         if (end - st < 1000) {
           return;
         }
         st = +new Date();
-        const cx = 2;
-        const cy = 2;
-        const mw = width / cx;
-        const mh = height / cy;
         const miniData = new Uint8Array(mw * mh * 4);
         for (let j = 0; j < mh; j += 1) {
           for (let i = 0; i < mw; i += 1) {
             for (let c = 0; c < 4; c++) {
               const miniIndex = (j * mw + i) * 4 + c;
-              // console.log(miniIndex);
               let v = 0;
               for (let k = 0; k < cy; k++) {
                 for (let p = 0; p < cx; p++) {
@@ -182,8 +183,8 @@ const Counter = () => {
             }
           }
         }
-        saveImg(miniData, mw, mh);
-        // setData(miniData);
+        // saveImg(miniData, mw, mh);
+        setData(miniData);
       };
       const { width, height } = await createNes({ rom, render });
       // console.log(width, height);
@@ -194,11 +195,29 @@ const Counter = () => {
   }, []);
   const getColor = (x: number, y: number) => {
     if (!data) return "white";
-    const i = (y * width/4 + x) * 4;
+    const i = (y * mw + x) * 4;
     const c = `rgb(${data[i]}, ${data[i + 1]}, ${data[i + 2]})`;
     return c;
   };
   // return <Text>1</Text>;
+  return (
+    <>
+      {Array(mh)
+        .fill(0)
+        .map((_, y) => (
+          <Box key={y} width={mw}>
+            {Array(mw)
+              .fill(0)
+              .map((_, x) => (
+                <Text key={y * mw + x} color={getColor(x, y)}>
+                  {BLOCK_CHAR}
+                </Text>
+              ))}
+          </Box>
+        ))}
+    </>
+  );
+
   return (
     <>
       {Array(height / 4)
@@ -219,41 +238,3 @@ const Counter = () => {
 };
 
 render(<Counter />);
-
-/*
-export const BLOCK_CHAR = "█";
-export const BLOCK_CHAR2 = "█";
-const log = process.stderr;
-async function main() {
-  let st = +new Date();
-  const rom = new Uint8Array(Buffer.from(island_3_base64, "base64"));
-  let _w = 0;
-  let _h = 0;
-  const render = async (data: Uint8Array) => {
-    let s = "";
-    let i = 0;
-    const size = _w * _h;
-    let w = 0;
-    let h = 0;
-    let c = 0;
-    while (c < size) {
-      s += chalk.rgb(data[i], data[i + 1], data[i + 2])(BLOCK_CHAR);
-      i += 4;
-      c += 1;
-      w++;
-      if (w % 10 === 0) {
-        s += "\n";
-        h++;
-        w = 0;
-      }
-      if (h > 4) break;
-    }
-    log.write(s);
-    readline.moveCursor(log, 0, -4);
-  };
-  const { width, height } = await createNes({ rom, render });
-  _w = width;
-  _h = height;
-}
-main();
-*/
